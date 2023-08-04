60342568fff8ebc1b46dd31b75d5ee78146e442c
Wil Wade <wil.wade@unfinished.com>
2023-08-04 13:04:04 -0400

    Cleanup

--- src/AuthErrorBoundary.ts
+++ src/AuthErrorBoundary.ts
@@ -21,7 +21,6 @@ class AuthErrorBoundary extends React.Component<
 
   static getDerivedStateFromError(error: Error) {
     console.error("getDerivedStateFromError", error);
-    debugger;
     // Update state so the next render will show the fallback UI.
     return { hasError: false };
   }
