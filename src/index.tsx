a9cd44b2166f91e0fecc0c07218ef04fc9a634ce
Wes Biggs <wes.biggs@amplica.io>
2023-10-04 23:16:09 -0500

    Relative paths for dependencies in POC. Minor changes.

--- src/index.tsx
+++ src/index.tsx
@@ -13,10 +13,17 @@ if (document.location.pathname.endsWith("/review")) {
       <ReviewApp />
     </React.StrictMode>,
   );
-} else {
+} else if (document.location.pathname.endsWith("/feed")) {
   root.render(
     <React.StrictMode>
       <App />
+    </React.StrictMode>,
+  );
+} else {
+  root.render(
+    <React.StrictMode>
+    <h1><a href="/review?href=https://www.etsy.com/listing/1292521772/melting-clock-salvador-dali-the&reference=%7B%22hello%22%3A%22world%22%7D&attributeSetType=dsnp%3A%2F%2F1%23OndcProofOfPurchase">Post a review</a></h1><br/>
+    <h1><a href="/feed">View your feed</a></h1>
     </React.StrictMode>,
   );
 }
